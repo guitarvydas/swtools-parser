744173ffce9464075f224cee9e69f4c750be370e
Paul Tarvydas <guitarvydas@gmail.com>
2019-07-22 10:16:49 -0400

    added jjj.lisp to experiment with packages and WRITE

---
+++ jjj.lisp
@@ -0,0 +1,19 @@
+(ql:quickload :esrap)
+
+(defpackage :empty)
+
+(in-package :empty)
+
+(cl:defun write-qualified (val)
+  (cl:write val))
+
+(cl:defun test ()
+  (cl:let ((cl:*print-escape* cl:t))
+    (write-qualified 'esrap:+)
+    (cl:terpri)
+    (write-qualified 'cl:+)
+    (cl:terpri)
+    (write-qualified '(cl:progn cl:nil))
+    (cl:values)))
+
+(empty::test)
