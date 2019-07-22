f5eb5ce6e1480c9bda86a09b59ec308a9537cf4e
Paul Tarvydas <guitarvydas@gmail.com>
2019-07-22 10:14:29 -0400

    added create-swtools-parser.lisp

--- create-swtools-parser.lisp
+++ create-swtools-parser.lisp
@@ -1,13 +1,16 @@
 ;; @create-swtools-parser creates the file swtools-parser.lisp (or the given string name) using PEG and ESRAP
 ;; from the grammar in parser.peg
 
-(defun @create-swtools-parser (&key (peg-input-filename nil) (output-lisp-filename nil))
-  (unless peg-input-filename
-    (error "no PEG filename specified"))
-  (unless output-lisp-filename
-    (error "no output filename specified"))
-  (let ((peg-str (file-to-string peg-input-filename)))
-    (let ((parser-code (esrap:parse 'peg-grammar::grammar peg-str)))
-      (with-open-file (outf output-lisp-filename :direction :output :if-exists :supersede)
-        (write parser-code :stream outf)
+(cl:in-package :swtools-parser)
+
+(cl:defun @create-swtools-parser (cl:&key (peg-input-filename nil) (output-lisp-filename nil))
+  (cl:unless peg-input-filename
+    (cl:error "no PEG filename specified"))
+  (cl:unless output-lisp-filename
+    (cl:error "no output filename specified"))
+  (cl:let ((peg-str (swtools-parser:file2string peg-input-filename)))
+    (cl:let ((parser-code (esrap:parse 'peg-grammar:peg-grammar peg-str)))
+      (cl:with-open-file (outf output-lisp-filename :direction :output :if-exists :supersede)
+	(empty:write-qualified parser-code outf)
 	'ok))))
+
